c5c2f68eb7419ab44b42bb2ff320eb7bc11c16aa
LennertPurnal <60964915+LennertPurnal@users.noreply.github.com>
2020-12-26 18:34:50 +0100

    voeg landbouwbedrijf toe toegevoegd

--- project-template/src/main/java/be/kuleuven/csa/controller/BeheerLandbouwbedrijvenController.java
+++ project-template/src/main/java/be/kuleuven/csa/controller/BeheerLandbouwbedrijvenController.java
@@ -4,12 +4,17 @@ import be.kuleuven.csa.model.databaseConn.CsaDatabaseConn;
 import be.kuleuven.csa.model.domain.Landbouwbedrijf;
 import com.sun.javafx.scene.control.IntegerField;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.control.cell.TextFieldTreeTableCell;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.util.converter.IntegerStringConverter;
+import org.w3c.dom.Text;
+
+import java.util.Optional;
 
 public class BeheerLandbouwbedrijvenController {
 
@@ -17,8 +22,6 @@ public class BeheerLandbouwbedrijvenController {
     private Button btnDelete;
     @FXML
     private Button btnAdd;
-    @FXML
-    private Button btnModify;
     @FXML
     private Button btnClose;
     @FXML
@@ -37,10 +40,7 @@ public class BeheerLandbouwbedrijvenController {
     public void initialize() {
         initTable();
         btnAdd.setOnAction(e -> addNewRow());
-        btnModify.setOnAction(e -> {
-            verifyOneRowSelected();
-            modifyCurrentRow();
-        });
+
         btnDelete.setOnAction(e -> {
             verifyOneRowSelected();
             deleteCurrentRow();
@@ -63,6 +63,7 @@ public class BeheerLandbouwbedrijvenController {
         bedrijfsNaam.setOnEditCommit(event -> {
             Landbouwbedrijf selectedBedrijf = event.getRowValue();
             selectedBedrijf.setNaam(event.getNewValue());
+            modifyCurrentRow();
         });
 
         bedrijfsGemeente.setCellValueFactory((new PropertyValueFactory<>("Gemeente")));
@@ -70,6 +71,7 @@ public class BeheerLandbouwbedrijvenController {
         bedrijfsGemeente.setOnEditCommit(event -> {
             Landbouwbedrijf selectedBedrijf = event.getRowValue();
             selectedBedrijf.setGemeente(event.getNewValue());
+            modifyCurrentRow();
         });
 
         bedrijfsPostcode.setCellValueFactory(new PropertyValueFactory<>("postcode"));
@@ -77,6 +79,7 @@ public class BeheerLandbouwbedrijvenController {
         bedrijfsPostcode.setOnEditCommit(event -> {
             Landbouwbedrijf selectedBedrijf = event.getRowValue();
             selectedBedrijf.setPostcode(Integer.valueOf(event.getNewValue()));
+            modifyCurrentRow();
         });
 
         bedrijfsOndernemingsNR.setCellValueFactory(new PropertyValueFactory<>("ondernemingsNR"));
@@ -84,6 +87,7 @@ public class BeheerLandbouwbedrijvenController {
         bedrijfsOndernemingsNR.setOnEditCommit(event -> {
             Landbouwbedrijf selectedBedrijf = event.getRowValue();
             selectedBedrijf.setOndernemingsNR(Integer.valueOf(event.getNewValue()));
+            modifyCurrentRow();
         });
 
         bedrijfsLand.setCellValueFactory(new PropertyValueFactory<>("land"));
@@ -91,6 +95,7 @@ public class BeheerLandbouwbedrijvenController {
         bedrijfsLand.setOnEditCommit(event -> {
             Landbouwbedrijf selectedBedrijf = event.getRowValue();
             selectedBedrijf.setLand(event.getNewValue());
+            modifyCurrentRow();
         });
 
         for (Landbouwbedrijf bedrijf: CsaDatabaseConn.getDatabaseConn().getCsaRepo().getLandbouwbedrijven()) {
@@ -102,6 +107,10 @@ public class BeheerLandbouwbedrijvenController {
     }
 
     private void addNewRow() {
+        var bedrijfToeTeVoegen = showAddNewRowDialog();
+        if (bedrijfToeTeVoegen.isPresent()){
+            CsaDatabaseConn.getDatabaseConn().getCsaRepo().persistRecord(bedrijfToeTeVoegen.get());
+        }
     }
 
     private void deleteCurrentRow() {
@@ -126,5 +135,60 @@ public class BeheerLandbouwbedrijvenController {
         }
     }
 
+    private Optional<Landbouwbedrijf> showAddNewRowDialog(){
+        Dialog<Landbouwbedrijf> dialog = new Dialog<>();
+        dialog.setTitle("Voeg Landbouwbedrijf toe");
+        dialog.setHeaderText("Voeg een nieuw Landbouwbedrijf toe, velden met een * zijn verplicht");
+
+        ButtonType voegToeButtonType = new ButtonType("Voeg toe", ButtonBar.ButtonData.OK_DONE);
+        dialog.getDialogPane().getButtonTypes().addAll(voegToeButtonType, ButtonType.CANCEL);
+
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(20, 150, 10, 10));
+
+        TextField naamtext = new TextField();
+        naamtext.setPromptText("Naam");
+        TextField gemeentetext = new TextField();
+        gemeentetext.setPromptText("Gemeente");
+        TextField postcodetext = new TextField();
+        postcodetext.setPromptText("Postcode");
+        TextField ondernemingsNRtext = new TextField();
+        ondernemingsNRtext.setPromptText("Ondernemingsnummer");
+        TextField landtext = new TextField();
+        landtext.setPromptText("Land");
+
+        grid.add(new Label("Naam*:"), 0, 0);
+        grid.add(naamtext, 1, 0);
+        grid.add(new Label("Gemeente*:"), 0, 1);
+        grid.add(gemeentetext, 1, 1);
+        grid.add(new Label("Postcode*:"), 0, 2);
+        grid.add(postcodetext, 1, 2);
+        grid.add(new Label("ondernemingsNR*:"), 0, 3);
+        grid.add(ondernemingsNRtext, 1, 3);
+        grid.add(new Label("Land"), 0, 4);
+        grid.add(landtext, 1, 4);
+        dialog.getDialogPane().setContent(grid);
+
+        dialog.setResultConverter(dialogButton -> {
+            if (dialogButton == voegToeButtonType){
+                Landbouwbedrijf newbedrijf = new Landbouwbedrijf(
+                        Integer.parseInt(ondernemingsNRtext.getText()),
+                        naamtext.getText(),
+                        gemeentetext.getText(),
+                        Integer.parseInt(postcodetext.getText()));
+                newbedrijf.setLand(landtext.getText());
+                return newbedrijf;
+
+            }
+            return null;
+        });
+
+
+
+        Optional<Landbouwbedrijf> bedrijfToeTeVoegen = dialog.showAndWait();
+        return bedrijfToeTeVoegen;
+    }
 
 }
